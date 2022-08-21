0c1786840b1de8831c9def5532902a849257f326
kidperk11 <intothescrapheap@gmail.com>
2022-08-21 18:02:00 -0400

    UPDATE: Added some scripts and changed the gitignore

---
+++ Assets/Scripts/MonsterAI/Behaviors/MoveTowards.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class MoveTowards : Action
+{
+   // The speed of the object
+   public float speed = 0; 
+   // The transform that the object is moving towards
+   public SharedTransform target;
+
+   public override TaskStatus OnUpdate()
+   {
+      // Return a task status of success once we've reached the target
+      if (Vector3.SqrMagnitude(transform.position - target.Value.position) < 0.1f) {
+         return TaskStatus.Success;
+      }
+      // We haven't reached the target yet so keep moving towards it
+      transform.position = Vector3.MoveTowards(transform.position, target.Value.position, speed * Time.deltaTime);
+      return TaskStatus.Running;
+   }
+}
